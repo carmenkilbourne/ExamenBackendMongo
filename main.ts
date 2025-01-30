090a9540c6b62663dd8f07bb4dcc07601b1beed6
carmenkilbourne <113438693+carmenkilbourne@users.noreply.github.com>
2025-01-30 09:45:21 +0100

    listo para el examen

--- main.ts
+++ main.ts
@@ -3,12 +3,23 @@ import { MongoClient, ObjectId } from "mongodb";
 const MONGO_URL =
   "mongodb+srv://ckilbourne:12345@nebrija-cluster.cumaf.mongodb.net/?retryWrites=true&w=majority&appName=Nebrija-Cluster";
 const client = new MongoClient(MONGO_URL);
-const dbName = "nebrijadb";
+const dbName = "Examen";
 await client.connect();
 console.log("Conexion exitosa");
 const db = client.db(dbName);
-//const cosaCollection = db.collection<cosaModel>("cosa");
+//const testCollection = db.collection<TestModel>("test");
 const handler = async (req: Request): Promise<Response> => {
+  const method = req.method;
+  const url = new URL(req.url);
+  const path = url.pathname;
+  if (method === "GET") {
+    if (path === "/hola") {
+      return new Response("hola",{status:200});
+    }
+  }
+  else if (method === "POST") {}
+  else if (method === "PUT") {}
+
   return new Response("No endpoint", { status: 404 });
 };
-Deno.serve({ port: 6768 }, handler);
+Deno.serve({ port: 4000 }, handler);
